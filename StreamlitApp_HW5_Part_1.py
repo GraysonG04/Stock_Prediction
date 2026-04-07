af97e60bb000c0494e498756745508d2c34f859e
GraysonG04 <g.l.garcia@tcu.edu>
2026-04-07 13:36:44 -0500

    Update StreamlitApp_HW5_Part_1.py

--- StreamlitApp_HW5_Part_1.py
+++ StreamlitApp_HW5_Part_1.py
@@ -31,7 +31,7 @@ project_root = os.path.abspath(os.path.join(current_dir, '..'))
 if project_root not in sys.path:
     sys.path.append(project_root)
 
-from src.feature_utils import convert_input_pca_regression
+from feature_utils import convert_input_pca_regression
 
 # Access the secrets
 aws_id = st.secrets["aws_credentials"]["AWS_ACCESS_KEY_ID"]
